527c627a4ed5aa04dc9ab4f8e080a3a5ec43cb12
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-06-10 00:51:17 +0200

    step-3

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render(){
     return (
         <div>
-          <h1>Hello People</h1>
-          <p>Here we are</p>
+          <h1>{this.props.text}</h1>
         </div>
     )
   }
 }
-export default App;
+
+App.propTypes = {
+  text: React.PropTypes.string,
+  value: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  text: 'This is the default'
+}
+
+ReactDOM.render(
+    <App text='this is my first property' value={10}/>,
+    document.getElementById('app')
+
+)
