0228dec1ad455bac8ccaf5f6e809053cd00b4a43
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-06-10 00:34:15 +0200

    step-2

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>Hello People</h1>
-    // JSX for
-    // return React.createElement('h1', null, 'Hello Everyone');
-    // Where the second argument are properties and the third argument is for inner element/string.
+    return (
+        <div>
+          <h1>Hello People</h1>
+          <p>Here we are</p>
+        </div>
+    )
   }
 }
-
-// stateless component alternative:
-// const App = () => <h1>Hello digilabs </h1>
-
 export default App;
