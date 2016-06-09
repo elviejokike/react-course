7f36c7a2212cde58debe5d1bebf1c3567c17bd89
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-06-10 01:13:14 +0200

    step-4

--- App.js
+++ App.js
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      text: "this is my state"
+    }
+
+  }
+  update(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
   render(){
     return (
         <div>
-          <h1>{this.props.text}</h1>
+          <h1>Hola, {this.state.text}</h1>
+          <input type="text" onChange={this.update.bind(this)}/>
         </div>
     )
   }
 }
 
-App.propTypes = {
-  text: React.PropTypes.string,
-  value: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  text: 'This is the default'
-}
-
-ReactDOM.render(
-    <App text='this is my first property' value={10}/>,
-    document.getElementById('app')
-
-)
+export default App;
