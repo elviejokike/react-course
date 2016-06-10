55a463a269b2be029a16b29d37c47135c742fd5a
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-06-10 16:04:14 +0200

    step-13 Dynamically Generated Components

--- App.js
+++ App.js
@@ -5,60 +5,34 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      text: '',
-      red: 0
+      data: [
+        {id:1, name: "Leo Messi"}, {id:2, name:"Roger Federer"},
+        {id:3, name: "Nairo Quinatana"}, {id:4, name:"Verstappen"}
+      ]
     };
-    this.update = this.update.bind(this);
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.input).value
-    })
   }
   render(){
+    let rows = this.state.data.map ( person => {
+      // key is needed for sibling elements
+      return <Person key={person.id} data={person}></Person>
+    })
     return (
-      <div>
-        <NumericInput ref="red" val={+this.state.red} min={0} max={255} step={1} label='Red' update={this.update}/>
-        <br/>
-      </div>
+      <table>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     )
   }
 }
 
-class NumericInput extends React.Component {
-  render(){
-    return (
-        <div>
-          <input ref="input" type={this.props.type}
-            min={this.props.min}
-            max={this.props.max}
-            step={this.props.step}
-            defaultValue={this.props.val}
-            onChange={this.props.update}>
-          </input>
-            <label>{this.props.label} - {this.props.val}</label>
-          </div>
-    );
-  }
-}
-
-NumericInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number','range'])
-}
-
-NumericInput.defaultProps = {
-  min: 0,
-  max: 0,
-  step:0,
-  val: 0,
-  label: '',
-  type: 'range'
+const Person = (props) => {
+  return (
+    <tr>
+      <td>{props.data.id}</td>
+      <td>{props.data.name}</td>
+    </tr>
+  )
 }
 
 export default App;
