27a4d6e66fc88a7c16da8529f63685a53a5169c0
Kike <enrique.jose.sanjuanelo.robles@philips.com>
2016-06-10 10:13:00 +0200

    step-8 Component Lifecycle - Mounting Basics

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {val : 0};
+    this.update = this.update.bind(this);
+  }
+  update(){
+    this.setState({val: this.state.val + 1});
+  }
+  // component is ready to be rendered (before placed into the DOM)
+  componentWillMount(){
+    console.log('mounting !');
+  }
+
   render(){
-    return <Button> Click to Pay <Heart/></Button>
+    console.log('rendering !');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+
+  // component has been placed into the DOM (Rendered)
+  componentDidMount(){
+    console.log('mounted !');
+  }
+
+  componentWillUnmount(){
+    console.log('bye component');
   }
 }
 
-class Button extends React.Component {
-  render(){
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(<App/>, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render() {
     return (
-        <button>{this.props.children}</button>
-    );
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id='a'></div>
+      </div>
+    )
   }
+
 }
 
-const Heart = () => <span>	&#9825; </span>
-
-export default App;
+export default Wrapper;
